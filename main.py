750e4de8c1d70d289db59f6ea4934eb37f7e982a
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-05-27 13:47:44 +0000

    Lista_2

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-#lista
+#Lista de tarefas
 
-frutas = ['Abacate','Banana','Morango']
-frutas.append('Uva')
-frutas.remove('Banana')
-print(len(frutas))
-print(frutas)
+tarefas = []
+
+tarefas.append('Ir trabalhar')
+tarefas.append('Estudar python')
+tarefas.append('Ir para o studio')
+
+print('Minha lista de tarefas: ')
+for tarefa in tarefas:
+    print(tarefa)
