a582a7e2efb38d49b0b458b1ee8cfe74201e75b3
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-05-28 13:24:18 +0000

    Tuplas

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-#Lista de tarefas
+#Tupla - dados imutaveis
 
-tarefas = []
-
-tarefas.append('Ir trabalhar')
-tarefas.append('Estudar python')
-tarefas.append('Ir para o studio')
-
-print('Minha lista de tarefas: ')
-for tarefa in tarefas:
-    print(tarefa)
+meses = ('janeiro', 'fevereiro')
+print(meses[1])
