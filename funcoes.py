8af2af5116aa51cc6956fe94a417a1563942a992
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-05-29 18:34:48 +0000

    Funções_5 - Modularização

--- funcoes.py
+++ funcoes.py
@@ -1,2 +1,8 @@
 def somar(num1, num2):
     return num1 + num2
+
+def saudacao(nome):
+    print(f'Olá, {nome}')
+
+def verificador_idade(idade):
+    return idade >= 18
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-from funcoes import somar
+from funcoes import somar, saudacao, verificador_idade
+saudacao('Pedro')
 print(somar(2,6))
+
+maior_de_idade = verificador_idade(int(input('Qual a sua idade? ')))
+print(f'É maior de idade? {maior_de_idade}')
