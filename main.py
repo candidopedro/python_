cda27be88c43c9518de550f0424f26e099cd2da3
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-05-29 19:07:53 +0000

    Objetos_1

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from funcoes import somar, saudacao, verificador_idade
-saudacao('Pedro')
-print(somar(2,6))
-
-maior_de_idade = verificador_idade(int(input('Qual a sua idade? ')))
-print(f'É maior de idade? {maior_de_idade}')
+class Casa:
+    def __init__(self):
+        
