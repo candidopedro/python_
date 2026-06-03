80d1b0cb6e03aeace646ec27bd33f12dacb5ab4b
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-06-03 19:37:14 +0000

    Objeto_teste

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-class Casa:
-    def __init__(self):
-        
+class Carro: #Sempre colocar a Primeira letra maiuscula, ex: CarroNovo
+    def __init__(self, cor, ano, marca):
+        self.cor = cor
+        self.ano = ano 
+        self.marca = marca
+    
+    def informacoes_veiculo(self):
+        print('Informações do veiculo:')
+        print(f'Cor: {self.cor}')
+        print(f'Ano: {self.ano}')
+        print(f'Marca: {self.marca}')
+
+carro1 = Carro('Vermelho', 2026, 'Ferrari')
+Carro.informacoes_veiculo()
