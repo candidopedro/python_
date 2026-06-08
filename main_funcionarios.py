ad4aa1b22af3e733d905aa326266515ee411830c
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-06-08 17:25:25 +0000

    Objeto_funcionarios

--- main_funcionarios.py
+++ main_funcionarios.py
@@ -2,27 +2,36 @@
 class Pessoa:
     def __init__(self, nome, idade, cargo):
         self.nome = nome
-        self.idade = idade
+        self.idade = int(idade)
         self.cargo = cargo
     
-    def promover(self):
-        self.cargo = input(f'Digite o novo cargo de {self.nome}: ')
-        print(f'{self.nome} foi promovido(a) para a nova função de {self.cargo}!\n')
+    def promover(self,novo_cargo):
+        print(f'{self.nome} foi promovido(a) para a nova função de {novo_cargo}!\n')
 
     def informacoes(self):
         print(f'- Nome: {self.nome}')
         print(f'- Idade: {self.idade}')
         print(f'- Cargo: {self.cargo}\n')
 
+    def nova_idade(self, nova_idade):
+        if nova_idade > self.idade:
+            self.idade = nova_idade
+            print(f'Idade de {self.nome} atualizada para {self.idade}')
+        return
+    
 colaborador1 = Pessoa('Pedro Henrique', 23, 'Dev Junior')
 colaborador2 = Pessoa('João Paulo', 43, 'Analista Pleno')
-
 
 colaborador1.informacoes()
 colaborador2.informacoes()
 
-colaborador1.promover()
-colaborador2.promover()
+#Atualização: 
+
+colaborador1.promover('Dev Pleno')
+colaborador2.promover('Analista Sênior')
+
+colaborador1.nova_idade(24)
+colaborador2.nova_idade(44)
 
 print('Dados atualizados: ')
 colaborador1.informacoes()
