adbc3d4eccf4c26fd93136c9b3c97de01d2834ea
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-06-08 13:29:19 +0000

    Objeto_carro

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 class Carro: #Sempre colocar a Primeira letra maiuscula, ex: CarroNovo
-    def __init__(self, cor, ano, marca, ligado):
+    def __init__(self, cor, ano, marca):
         self.cor = cor
         self.ano = ano 
         self.marca = marca
-        self.ligado = bool(ligado)
+        self.ligado = True
     
     def informacoes_veiculo(self):
         print('Informações do veiculo:')
         print(f'Cor: {self.cor}')
         print(f'Ano: {self.ano}')
         print(f'Marca: {self.marca}')
-        print(f'Está ligado: {self.ligado}')
+    
+    def ligar(self):
+        if not self.ligado:
+            self.ligado = True
+            print('O carro foi ligado')
+        else:
+            print('O carro já está ligado')
 
-carro1 = Carro('Vermelho', 2026, 'Ferrari', True) #Passando as informações para caracterizar objeto
+
+carro1 = Carro('Vermelho', 2026, 'Ferrari') #Passando as informações para caracterizar objeto
 carro1.informacoes_veiculo() #Imprimindo as informações atribuidas ao objeto
+carro1.ligar()
