3d3f58b2db693976b386797d5458156435845fe5
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-06-08 20:20:46 +0000

    Henrança funcionários

---
+++ main_hernanca_1.py
@@ -0,0 +1,16 @@
+class Pessoa:
+    def __init__(self,nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def apresentar(self):
+        print(f'Olá, meu nome é {self.name} e tenho {self.idade} anos')
+
+class Funcionario:
+    pass
+
+class Cliente:
+    pass
+
+
+        
