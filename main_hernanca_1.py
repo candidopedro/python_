96500c7b2990fe6b8b76ba9079d2361da3e13178
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-06-09 18:55:29 +0000

    Herança cliente/funcionário

--- main_hernanca_1.py
+++ main_hernanca_1.py
@@ -6,11 +6,21 @@ class Pessoa:
     def apresentar(self):
         print(f'Olá, meu nome é {self.name} e tenho {self.idade} anos')
 
-class Funcionario:
-    pass
+class Funcionario(Pessoa):
+    def __init__(self, nome, idade, cargo):
+        super().__init__(nome, idade) #Puxa as informações da Class Pai
+        self.cargo = cargo
 
-class Cliente:
-    pass
+class Cliente(Pessoa):
+    def __init__(self, nome, idade, saldo):
+        super().__init__(nome, idade)
+        self.saldo = saldo
 
-
-        
+    def comprar(self,valor_produto):
+        if self.saldo >= valor_produto:
+            print(f'Parabéns {self.nome}!')
+            print(f'Item de {valor_produto} adquirido!')
+        else:
+            print('Saldo insuficiente!')
+    
+cliente1 = Cliente.comprar('')
