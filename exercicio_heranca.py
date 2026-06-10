c2e5b028d8de360f675a0cec1c1fffb3208bca36
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-06-10 20:42:43 +0000

    Exercício teste Multniviel

---
+++ exercicio_heranca.py
@@ -0,0 +1,19 @@
+#Faça a atribuição de peças compartilhadas no grupo Volkswagen
+
+class GrupWagen:
+    def __init__(self, marca, modelo, motor):
+        self.marca = marca
+        self.modelo = modelo
+        self.motor = motor
+
+class CaynneTurboGt(GrupWagen):
+    pass
+
+class RsQ8(GrupWagen):
+    pass
+
+class Bentayga(GrupWagen):
+    pass
+
+class EA825():
+    
