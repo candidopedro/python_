ea46e2b4fd66dcb16fcecce60e62ea9e0a8e8ed9
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-06-12 17:55:04 +0000

    Usando o Super/if em uma linha/nomeando parâmetros

--- usando_super.py
+++ usando_super.py
@@ -3,49 +3,59 @@
 #Cada professor deve ter nome, idade e materia
 
 class Pessoa:
-    def __init__(self, nome, idade):
+    def __init__(self, nome, idade, status):
         self.nome = nome 
         self.idade = idade
+        self.status = status
+
+    def status_pessoa(self):
+        print(f'- Status: {"Ativo\n" if self.status == True else "Inativo\n"}') # <--- Aqui foi posto uma condicional em apenas uma linha
 
     def apresentar(self): # esta def existe na class Filho também - OBS
-        print(f'É um teste {self.nome}! :)\n')  #Para mostrar que a class Filho sobrepoe a class Pai - OBS
+        print(f'- É um teste, {self.nome}! :)\n')  #Para mostrar que a class Filho sobrepoe a class Pai - OBS
 
 class Aluno(Pessoa):
-    def __init__(self, nome, idade, matricula):
-        super().__init__(nome, idade)
+    def __init__(self, nome, idade, status, matricula):
+        super().__init__(nome, idade, status)
         self.matricula = matricula
 
     def apresentar(self): #Esta está sobrepondo a class Pai - OBS
-        super().apresentar() # <------------ Este "apresentar" vem da class Pai "Pessoa" e aparecerá apenas para os Alunos
         print(f'Olá Aluno(a) {self.nome}!\n- Sua matrícula é: {self.matricula}\n')
+        super().status_pessoa()
+        super().apresentar() # <------------ Este "apresentar" vem da class Pai "Pessoa" e aparecerá apenas para os Alunos
 
 class Professor(Pessoa):
-    def __init__(self, nome, idade, materia):
-        super().__init__(nome, idade)
+    def __init__(self, nome, idade, status, materia):
+        super().__init__(nome, idade, status)
         self.materia = materia
     
     def apresentar(self):
         print(f'Olá Professor(a) {self.nome}!\n- A(s) matéria(s) que você leciona: {self.materia}\n')
+        super().status_pessoa()
 
 class Assistente(Pessoa):
-    def __init__(self, nome, idade, bloco):
-        super().__init__(nome, idade)
+    def __init__(self, nome, idade, status, bloco):
+        super().__init__(nome, idade, status)
         self.bloco = bloco
 
     def apresentar(self):
         print(f'Olá Assistente {self.nome}!\n- Você está com o bloco: {self.bloco}\n')
+        super().status_pessoa()
 
-aluno1 = Aluno('Pedro', 23, 14646789)
+#Alunos
+aluno1 = Aluno(nome='Pedro', idade=23, status=True, matricula=14646789)# <---  Especificando os parâmetros para faciliar a LEITURA. Ex: nome='Pedro'
 aluno1.apresentar()
-aluno2 = Aluno('Isaque', 21, 43786322)
+aluno2 = Aluno(nome='Isaque', idade=21, status=False, matricula=43786322,)
 aluno2.apresentar()
 
-professor1 = Professor('Mauro', 54, 'Matemática')
+#Professores
+professor1 = Professor(nome='Mauro', idade=54, status=False, materia='Matemática')
 professor1.apresentar()
-professor2 = Professor('Karla', 45, 'Biologia')
+professor2 = Professor(nome='Karla', idade=45, status=True, materia='Biologia')
 professor2.apresentar()
 
-assistente1 = Assistente('Henrique', 25, 16)
+#Assistentes
+assistente1 = Assistente(nome='Henrique', idade=25, status=True, bloco=16)
 assistente1.apresentar()
-assistente2 = Assistente('Ana', 23, 18)
+assistente2 = Assistente(nome='Ana', idade=23, status=True, bloco=18)
 assistente2.apresentar()
