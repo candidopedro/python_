c76a7fa73087ef0e0f25aea2e66416fc015b1304
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-07-02 20:16:34 +0000

    nested

--- nested_aninhadas.py
+++ nested_aninhadas.py
@@ -1,1 +1,18 @@
 #Classes com o mesmo nome mas em arquivos diferente
+
+# Ainda não identifiquei o por que -->   ---
+
+class Computador:
+    def __init__(self, modelo, gpu_nome, modelo_gpu, vram):
+        self.modelo = modelo
+        self.gpu_nome = gpu_nome # ---
+        self.gpu = self.GPU(modelo_gpu, vram) # ---
+    
+    def mostrar_configuracoes(self):
+        print (f'Modelo: {self.modelo}')
+        print (f'')
+
+    class GPU: # Nested class - Uma classe dentro da outra 
+        def __init__(self, modelo_gpu, vram):
+            self.modelo_gpu = modelo_gpu
+            self.vram = vram    
