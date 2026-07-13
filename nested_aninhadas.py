d002e7bdb9f57c4a6057f58e90f67b86568f2e93
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-07-13 14:37:25 +0000

    Nested class - CPU

--- nested_aninhadas.py
+++ nested_aninhadas.py
@@ -1,15 +1,16 @@
 #Classes com o mesmo nome mas em arquivos diferente
-
 # Ainda não identifiquei o por que -->   ---
 
 class Computador:
-    def __init__(self, modelo, gpu_nome, modelo_gpu, vram):
+    def __init__(self, modelo, modelo_gpu, vram, marca_cpu, modelo_cpu, nucleos_cpu):
         self.modelo = modelo
-        self.gpu_nome = gpu_nome # ---
-        self.gpu = self.GPU(modelo_gpu, vram) # ---
+        self.gpu = self.GPU(modelo_gpu, vram) # Importa as informações da Class GPU e atribui a um objeto
+        self.cpu = self.CPU(marca_cpu, modelo_cpu, nucleos_cpu) # Importa as informações da Class GPU e atribui a um objeto
     
-    def mostrar_configuracoes_(self):
+    def mostrar_configuracoes(self):
         print(f'Computador: {self.modelo}')
+        self.gpu.mostrar_gpu()
+        self.cpu.mostar_cpu()
 
     class GPU: # Nested class - Uma classe dentro da outra 
         def __init__(self, modelo_gpu, vram):
@@ -17,4 +18,18 @@ class Computador:
             self.vram = vram    
         
         def mostrar_gpu(self):
-            print(f'')
+            print(f'Placa de vídeo: {self.modelo_gpu}')
+            print(f'Quantidade de VRAM: {self.vram}GB')
+
+    class CPU: # Nested class - Uma classe dentro da outra 
+        def __init__(self, marca_cpu, modelo_cpu, nucleos_cpu):
+            self.marca_cpu = marca_cpu
+            self.modelo_cpu = modelo_cpu
+            self.nucleos_cpu = nucleos_cpu
+        
+        def mostar_cpu(self):
+            print(f'CPU: {self.marca_cpu} {self.modelo_cpu}')
+            print(f' - Quantidade de núcleos: {self.nucleos_cpu}')
+
+computador_1 = Computador('DELL XPS', 'RTX 5090', 12, 'Intel', 'i7-4790', 8)
+Computador.mostrar_configuracoes(computador_1)
