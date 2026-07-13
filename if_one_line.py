af47ec446b56e4a9346eeb396725d4f4c8ee0d7e
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-07-13 20:38:34 +0000

    While loops

---
+++ if_one_line.py
@@ -0,0 +1,5 @@
+# Se for maior de 16 anos pode votar, se não o direito ao voto será anulado 
+
+years_old = 16
+
+print(f'Parabéns! Você é apto para votar!' if years_old > 16 else 'Você não é apto para votar') 
---
+++ while_loops.py
@@ -0,0 +1,9 @@
+# Somos donos de uma loja online, que possui vendedores tercerizados - assim com a shopee. Mas precisamos ter uma margem de lucro de 10% sobre cada produto postados por eles se maior que R$20
+
+value_product = 0
+
+print(f'{'='*15} AMAZON {'='*15}\n')
+
+while value_product <= 20:
+    value_product = float(input('Digite o valor do produto: R$')) 
+print(f' - Lucro obtido por unidade: R${value_product-(value_product*0.10)}')
