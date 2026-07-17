07acbebb6cb4ecb8c1edebca90a73e79b3b316a2
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-07-17 12:01:44 +0000

    organization of the dictionary

---
+++ dictionary_organization.py
@@ -0,0 +1,11 @@
+# A way to organize the dictionary
+aluno = {
+    'nome':'João', 
+    'idade': 14, 
+    'media': 'B', 
+    'aprovacao': True
+}
+
+print(aluno.keys())
+print(aluno.values())
+print(aluno.items())
