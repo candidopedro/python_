87a78d9df553fd9c2dfbcdf9e1435a1068c0df84
Pedro Henrique <95263125+candidopedro@users.noreply.github.com>
2026-07-27 16:59:31 +0000

    Explicando o 'item' antes do for

--- for_in_list.py
+++ for_in_list.py
@@ -7,5 +7,5 @@ for item in veiculos:
 
 print(veiculos_byd)
 
-veiculos_byd2 = [item for item in veiculos if 'BYD' in item] # <-- Foi usado um 'for' dentro da lista
+veiculos_byd2 = [item.upper() for item in veiculos if 'BYD' in item] # <-- Foi usado um 'for' dentro da lista / 'item.upper()' é um exemplo para mostrar que alterando aquele 'item', os itens atribuidos a lista serão alterados
 print(veiculos_byd2)
